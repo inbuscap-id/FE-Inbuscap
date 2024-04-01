e82773370304c225004656839d4117a87663d075
adeprastyo <adeprastyo101@gmail.com>
2024-04-01 21:32:42 +0700

    fix: bug after merge and fix slicing in auth

--- src/utils/apis/auth/types.ts
+++ src/utils/apis/auth/types.ts
@@ -8,36 +8,26 @@ export const loginSchema = z.object({
   password: z.string().min(1, { message: "Password is required" }),
 });
 
-export const registerSchema = z
-  .object({
-    fullname: z.string().min(1, { message: "Full name is required" }),
-    email: z
-      .string()
-      .min(1, { message: "Email is required" })
-      .email("Not a valid email"),
-    handphone: z
-      .string()
-      .min(8, { message: "Phone Number minimum length is 8" }),
-    password: z
-      .string()
-      .min(6, { message: "Password must be at least 6 characters" }),
-    ktp: z
-      .string()
-      .min(16, { message: "Ktp Number minimum length is 16" }),
-    npwp: z
-      .string()
-      .min(8, { message: "Phone Number minimum length is 16" }),
-    
-  })
+export const registerSchema = z.object({
+  fullname: z.string().min(1, { message: "Full name is required" }),
+  email: z
+    .string()
+    .min(1, { message: "Email is required" })
+    .email("Not a valid email"),
+  handphone: z.string().min(8, { message: "Phone Number minimum length is 8" }),
+  password: z
+    .string()
+    .min(6, { message: "Password must be at least 6 characters" }),
+  ktp: z.string().min(16, { message: "Ktp Number minimum length is 16" }),
+  npwp: z.string().min(8, { message: "Phone Number minimum length is 16" }),
+});
 
-  export const verificationSchema = z
-  .object({
-    photo_ktp: z.string().min(1, {message: "Upload your photo first"}),
-    photo_npwp: z.string().min(1, {message: "Upload your photo first"}),
-    photo_selfie: z.string().min(1, {message: "Upload your photo first"}),
+export const verificationSchema = z.object({
+  photo_ktp: z.string().min(1, { message: "Upload your photo first" }),
+  photo_npwp: z.string().min(1, { message: "Upload your photo first" }),
+  photo_selfie: z.string().min(1, { message: "Upload your photo first" }),
+});
 
-  });
-  
 export type LoginType = z.infer<typeof loginSchema>;
 export type RegisterType = z.infer<typeof registerSchema>;
-export type  VerificationType = z.infer<typeof verificationSchema>;
+export type VerificationType = z.infer<typeof verificationSchema>;
